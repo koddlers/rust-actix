c79849c2d2d8d465843b9986976f2c45ae97dd26
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-08-11 17:12:12 +0600

    Refactor `ApiResponse`
    * remove `pub` from members of `ApiResponse`
    * remove member `ApiResponse.status_code`
    * print the `HttpRequest` object and get `host` from the header

--- src/utils/api_response.rs
+++ src/utils/api_response.rs
@@ -3,15 +3,13 @@ use actix_web::{web, HttpRequest, HttpResponse, Responder};
 use actix_web::body::BoxBody;
 
 pub struct ApiResponse {
-    pub status_code: u16,
-    pub body: String,
+    body: String,
     response_code: StatusCode,
 }
 
 impl ApiResponse {
     pub fn new(status_code: u16, body: String) -> Self {
         Self {
-            status_code,
             body,
             response_code: StatusCode::from_u16(status_code).unwrap(),
         }
@@ -21,7 +19,11 @@ impl ApiResponse {
 impl Responder for ApiResponse {
     type Body = BoxBody;
 
-    fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
+    fn respond_to(self, req: &HttpRequest) -> HttpResponse<Self::Body> {
+        println!("request: {:?}", req);
+        let host = req.headers().get("host").unwrap().to_owned();
+        println!("host: {:?}", host);
+
         let body = BoxBody::new(web::BytesMut::from(self.body.as_bytes()));
         HttpResponse::new(self.response_code).set_body(body)
     }
