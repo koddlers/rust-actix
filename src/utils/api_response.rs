db2960bbc384ef3298d8c0f11f05aba2848a1f8b
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-08-13 21:54:10 +0600

    Implement `Display` for `ApiResponse`

--- src/utils/api_response.rs
+++ src/utils/api_response.rs
@@ -1,7 +1,9 @@
+use std::fmt::{Debug, Display, Formatter};
 use actix_web::http::StatusCode;
-use actix_web::{web, HttpRequest, HttpResponse, Responder};
+use actix_web::{web, HttpRequest, HttpResponse, Responder, ResponseError};
 use actix_web::body::BoxBody;
 
+#[derive(Debug)]
 pub struct ApiResponse {
     body: String,
     response_code: StatusCode,
@@ -26,5 +28,23 @@ impl Responder for ApiResponse {
 
         let body = BoxBody::new(web::BytesMut::from(self.body.as_bytes()));
         HttpResponse::new(self.response_code).set_body(body)
+    }
+}
+
+
+impl Display for ApiResponse {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Error: {} \n Status Code: {}", self.body, self.status_code())
+    }
+}
+
+impl ResponseError for ApiResponse {
+    fn status_code(&self) -> StatusCode {
+        self.response_code
+    }
+
+    fn error_response(&self) -> HttpResponse<BoxBody> {
+        let body = BoxBody::new(web::BytesMut::from(self.body.as_bytes()));
+        HttpResponse::new(self.status_code()).set_body(body)
     }
 }
