38ec5f7c40e1a8c96c7c077fc7c1a73b85c2d5bd
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-08-14 22:23:51 +0600

    Add error handling to `home_handler`

--- src/routes/handlers/home_handler.rs
+++ src/routes/handlers/home_handler.rs
@@ -1,17 +1,20 @@
 use actix_web::{get, web, Responder};
 use sea_orm::{ConnectionTrait, Statement};
 use sea_orm::DatabaseBackend::Postgres;
-use crate::utils::api_response;
+use crate::utils::api_response::ApiResponse;
 use crate::utils::app_state::AppState;
 
 #[get("/hello/{name}")]
 pub async fn greet(name: web::Path<String>) -> impl Responder {
-    api_response::ApiResponse::new(200, format!("Hello {name}!"))
+    ApiResponse::new(200, format!("Hello {name}!"))
 }
 
 #[get("/test")]
-pub async fn test(app_state: web::Data<AppState>) -> impl Responder {
+pub async fn test(app_state: web::Data<AppState>) -> Result<ApiResponse, ApiResponse> {
     let _res = app_state.db
-        .query_all(Statement::from_string(Postgres, "select * from user;")).await.unwrap();
-    api_response::ApiResponse::new(200, "Test".to_string())
+        .query_all(Statement::from_string(Postgres, "select * from user;"))
+        .await
+        .map_err(|err| ApiResponse::new(500, err.to_string()))?;
+
+    Ok(ApiResponse::new(200, "Test".to_string()))
 }
