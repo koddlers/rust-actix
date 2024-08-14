93c01698b3afb602a489048c27a20f01e3e1b4ba
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-08-15 04:03:58 +0600

    Insert claims data into request

--- src/routes/middlewares/auth_middleware.rs
+++ src/routes/middlewares/auth_middleware.rs
@@ -1,8 +1,9 @@
 use crate::utils::api_response::ApiResponse;
+use crate::utils::jwt::decode_jwt;
 use actix_web::body::MessageBody;
 use actix_web::dev::{ServiceRequest, ServiceResponse};
 use actix_web::http::header::AUTHORIZATION;
-use actix_web::Error;
+use actix_web::{Error, HttpMessage};
 use actix_web_lab::middleware::Next;
 
 pub async fn check_auth_middleware(
@@ -14,9 +15,17 @@ pub async fn check_auth_middleware(
         return Err(Error::from(ApiResponse::new(401, String::from("Unauthorized"))));
     }
 
-    // TODO: this panics. fix this
-    // let token = auth.unwrap().to_str().unwrap().replace("Bearer ", "").to_owned();
-    // let _claim = decode_jwt(token).unwrap();
+    let token = auth.unwrap().to_str().unwrap().replace("Bearer ", "").to_owned();
+    // print!("{token}");
+    let claim = decode_jwt(token);
+    match claim {
+        Ok(_claim) => {
+            // print!("Claim: {:?}", _claim);
+            request.extensions_mut().insert(_claim.claims);
+        }
+        Err(err) => println!("Error in claim: {err:?}"),
+    }
+
 
     next.call(request).await.map_err(
         |err| Error::from(ApiResponse::new(500, err.to_string()))
