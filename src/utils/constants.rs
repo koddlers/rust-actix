bb99e07c8e3c63353f4345568bcda8568f38b006
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-08-14 21:45:05 +0600

    Add error handling and set application defaults

--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::string::ToString;
 use lazy_static::lazy_static;
 
 lazy_static! {
@@ -8,22 +9,31 @@ lazy_static! {
     pub static ref SECRET: String = set_secret();
 }
 
+// Application defaults
+const _HOST: &str = "localhost";
+const _PORT: &str = "5050";
+const _SECRET: &str = "961814da78737d7df5d994ace36ca7641c1933df3af7bef90bc0b77e9d4965f4";
+
+
 fn set_address() -> String {
     dotenv::dotenv().ok();
-    env::var("ADDRESS").unwrap()
+    env::var("ADDRESS").unwrap_or(_HOST.to_string())
 }
 
 fn set_port() -> u16 {
     dotenv::dotenv().ok();
-    env::var("PORT").unwrap().parse::<u16>().unwrap()
+    env::var("PORT")
+        .unwrap_or(_PORT.to_string()).parse::<u16>()
+        .expect("Cannot parse the port to serve the application on")
 }
 
 fn set_db() -> String {
     dotenv::dotenv().ok();
-    env::var("DATABASE_URL").unwrap()
+    env::var("DATABASE_URL")
+        .expect("ERROR: `DATABASE_URL` is not set")
 }
 
 fn set_secret() -> String {
     dotenv::dotenv().ok();
-    env::var("SECRET").unwrap()
+    env::var("SECRET").unwrap_or(_SECRET.to_string())
 }
