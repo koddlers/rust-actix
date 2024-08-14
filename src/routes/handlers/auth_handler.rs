77dd5eb1e482ef562e74a13a78cf9b6b16a6b9d5
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-08-14 22:20:41 +0600

    Add error handling to `auth_handler`

--- src/routes/handlers/auth_handler.rs
+++ src/routes/handlers/auth_handler.rs
@@ -2,13 +2,13 @@ use crate::utils::api_response::ApiResponse;
 use crate::utils::app_state::AppState;
 
 use actix_web::web::{Data, Json};
-use actix_web::{post, Responder};
+use actix_web::post;
 
+use crate::utils::jwt::encode_jwt;
 use entity::user::ActiveModel;
 use sea_orm::{ActiveModelTrait, ColumnTrait, Condition, EntityTrait, QueryFilter, Set};
 use serde::{Deserialize, Serialize};
 use sha256::digest;
-use crate::utils::jwt::encode_jwt;
 
 #[derive(Serialize, Deserialize)]
 struct RegisterModel {
@@ -24,33 +24,34 @@ struct LoginModel {
 }
 
 #[post("/register")]
-pub async fn register(app_state: Data<AppState>, data: Json<RegisterModel>) -> impl Responder {
+pub async fn register(app_state: Data<AppState>, data: Json<RegisterModel>) -> Result<ApiResponse, ApiResponse> {
     let user = ActiveModel {
         name: Set(data.name.clone()),
         email: Set(data.email.clone()),
         password: Set(digest(&data.password)),
         ..Default::default()
-    }.insert(&app_state.db).await.unwrap();
+    }.insert(&app_state.db)
+        .await
+        .map_err(|err| ApiResponse::new(500, err.to_string()))?;
 
     // TODO: return the newly created `user` without `password`
-    ApiResponse::new(200, format!("{}", user.id))
+    Ok(ApiResponse::new(200, format!("{}", user.id)))
 }
 
 #[post("/login")]
-pub async fn login(app_state: Data<AppState>, data: Json<LoginModel>) -> impl Responder {
+pub async fn login(app_state: Data<AppState>, data: Json<LoginModel>) -> Result<ApiResponse, ApiResponse> {
     let user = entity::user::Entity::find()
         .filter(
             Condition::all()
                 .add(entity::user::Column::Email.eq(&data.email))
                 .add(entity::user::Column::Password.eq(digest(&data.password)))
-        ).one(&app_state.db).await.unwrap();
+        ).one(&app_state.db)
+        .await
+        .map_err(|err| ApiResponse::new(500, err.to_string()))?
+        .ok_or(ApiResponse::new(401, String::from("User not found")))?;
 
-    if user.is_none() {
-        return ApiResponse::new(401, String::from("User not found"));
-    }
+    let token = encode_jwt(user.email, user.id)
+        .map_err(|err| ApiResponse::new(500, err.to_string()))?;
 
-    let user_data = user.unwrap();
-    let token = encode_jwt(user_data.email, user_data.id).unwrap();
-
-    ApiResponse::new(200, format!("{{ 'token': '{}' }}", token))
+    Ok(ApiResponse::new(200, format!("{{ 'token': '{}' }}", token)))
 }
